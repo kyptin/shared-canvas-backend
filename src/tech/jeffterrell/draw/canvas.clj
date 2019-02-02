4ca524eeffbfe65cea62028ce00f8b596dbef0fd
Jeff Terrell <terrell@cs.unc.edu>
2019-02-01 20:09:35 -0500

    Split canvas-related stuff into separate namespace

---
+++ src/tech/jeffterrell/draw/canvas.clj
@@ -0,0 +1,26 @@
+(ns tech.jeffterrell.draw.canvas
+  (:require [clojure.spec.alpha :as s])
+  (:import javax.imageio.ImageIO
+           [java.io ByteArrayInputStream ByteArrayOutputStream]
+           [java.awt Color Rectangle]
+           [java.awt.image BufferedImage]))
+
+(def width 800)
+(def height 600)
+
+(defn new-canvas []
+  (BufferedImage. width height BufferedImage/TYPE_INT_RGB))
+
+(defn draw-rect
+  [canvas x y width height red green blue]
+  (doto (.createGraphics canvas)
+    (.setPaint (Color. (float red) (float green) (float blue)))
+    (.draw (Rectangle. x y width height))))
+
+(defn canvas-as-png-response-map
+  [status canvas]
+  (let [buffer (ByteArrayOutputStream.)]
+    (ImageIO/write canvas "PNG" buffer)
+    {:status status
+     :headers {"Content-Type" "image/png"}
+     :body (ByteArrayInputStream. (.toByteArray buffer))}))
--- src/tech/jeffterrell/draw/main.clj
+++ src/tech/jeffterrell/draw/main.clj
@@ -3,58 +3,52 @@
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
             [clojure.spec.alpha :as s]
-            [ring.adapter.jetty :as jetty])
-  (:import javax.imageio.ImageIO
-           [java.io ByteArrayInputStream ByteArrayOutputStream]
-           [java.awt Color Rectangle]
-           [java.awt.image BufferedImage]))
+            [ring.adapter.jetty :as jetty]
+            [tech.jeffterrell.draw.canvas :as canvas]))
 
-(def width 800)
-(def height 600)
-
-(defonce canvas
-  (BufferedImage. width height BufferedImage/TYPE_INT_RGB))
+(defonce canvas (canvas/new-canvas))
 
 (s/def ::color-component (s/and float? #(<= 0.0 % 1.0)))
 (s/def ::color (s/tuple ::color-component ::color-component ::color-component))
 (s/def ::rect (s/tuple nat-int? nat-int? nat-int? nat-int? ::color))
 
-(defn draw-rect [request]
-  (let [body (try
-               (-> request :body slurp edn/read-string)
-               (catch Throwable _ nil))]
-    (if-not body
-      {:status 400, :body "could not read request body as edn"}
-      (let [rect-data (s/conform ::rect body)]
+(defn handle-rect-request
+  [request]
+  (let [body-string (-> request :body slurp)
+        body-data (try
+                    (edn/read-string body-string)
+                    (catch Throwable _ nil))]
+    (if-not body-data
+      {:status 400,
+       :body (str "Could not read input string '" body-string "' as edn.\n"
+                  "Try something like this instead:\n"
+                  "[50 100 10 30 [0.95 0.5 0.1]]\n"
+                  "(i.e. x, y, width, height, and RGB color\n)")}
+
+      (let [rect-data (s/conform ::rect body-data)]
         (if (= ::s/invalid rect-data)
           {:status 400
            :body (str "could not read request body as rect data; "
                       "spec output follows:\n"
-                      (s/explain-str ::rect body)
+                      (s/explain-str ::rect body-data)
                       \newline
-                      (pr-str (s/explain-data ::rect body)))}
+                      (pr-str (s/explain-data ::rect body-data)))}
 
           (let [[x y width height color] rect-data
                 [red green blue] color]
-            (doto (.createGraphics canvas)
-              (.setPaint (Color. (float red) (float green) (float blue)))
-              (.draw (Rectangle. x y width height)))
+            (canvas/draw-rect canvas x y width height red green blue)
+            {:status 200}
             {:status 200}))))))
-
-(defn image-body []
-  (let [buffer (ByteArrayOutputStream.)]
-    (ImageIO/write canvas "PNG" buffer)
-    (ByteArrayInputStream. (.toByteArray buffer))))
 
 (defn handler [request]
   (let [method (:request-method request)
         path (:uri request)]
     (cond
       (and (= method :get) (= path "/image"))
-      {:status 200, :headers {"Content-Type" "image/png"}, :body (image-body)}
+      (canvas/canvas-as-png-response-map 200 canvas)
 
       (and (= method :post) (= path "/rect"))
-      (draw-rect request)
+      (handle-rect-request request)
 
       :else {:status 404})))
 
@@ -77,75 +71,3 @@
   (start-server!)
   (stop-server!)
   )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-;; simple counter example actually used in class
-;; (ns tech.jeffterrell.draw-backend
-;;   (:gen-class)
-;;   (:require [clojure.edn :as edn]
-;;             [clojure.java.io :as io]
-;;             [clojure.spec.alpha :as s]
-;;             [ring.adapter.jetty :as jetty]
-;;             [clojure.java.io :as io]
-;;             [clojure.pprint :refer [pprint]]))
-
-;; (def counter (atom 0))
-
-;; (defn handler [request]
-;;   (let [path (:uri request)
-;;         method (:request-method request)]
-
-;;     (cond
-;;       (and (= method :get) (= path "/value"))
-;;       {:status 200
-;;        :headers {"Content-Type" "application/edn"}
-;;        :body (pr-str {:counter @counter})}
-
-;;       (and (= method :put) (= path "/value"))
-;;       (let [new-value (-> request :body slurp Integer/parseInt)]
-;;         (reset! counter new-value)
-;;         {:status 200
-;;          :headers {"Content-Type" "application/edn"}
-;;          :body (pr-str {:counter @counter})})
-
-;;       (= path "/inc")
-;;       {:status 200
-;;        :headers {"Content-Type" "application/edn"}
-;;        :body (do
-;;                (swap! counter inc)
-;;                (pr-str {:counter @counter}))}
-
-;;       :else {:status 404})))
-
-;; (defn start-server!
-;;   ([] (start-server! false))
-;;   ([join?]
-;;    (def server
-;;      (jetty/run-jetty handler {:port (or (System/getenv "PORT") 4002)
-;;                                :join? join?}))))
-
-;; (defn stop-server! []
-;;   (.stop server)
-;;   (def server nil))
-
-;; (defn -main
-;;   [& args]
-;;   (start-server! true))
