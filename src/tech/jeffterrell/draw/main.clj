b590f2123fdbed7ef479f33867a0091110bc53c2
Jeff Terrell <terrell@cs.unc.edu>
2019-02-02 20:07:33 -0500

    Use macros to clean up main rect request handler

--- src/tech/jeffterrell/draw/main.clj
+++ src/tech/jeffterrell/draw/main.clj
@@ -8,37 +8,42 @@
 
 (defonce canvas (canvas/new-canvas))
 
+(defmacro with-verified-edn-body [request [edn-data-name] & body]
+  `(let [body-string# (-> ~request :body slurp)
+         ~edn-data-name (try
+                          (edn/read-string body-string#)
+                          (catch Throwable _# nil))]
+     (if-not ~edn-data-name
+       {:status 400,
+        :body (str "Could not read input string '" body-string# "' as edn.\n"
+                   "Try something like this instead:\n"
+                   "[50 100 10 30 [0.95 0.5 0.1]]\n"
+                   "(i.e. x, y, width, height, and RGB color\n)")}
+       (do ~@body))))
+
 (s/def ::color-component (s/and float? #(<= 0.0 % 1.0)))
 (s/def ::color (s/tuple ::color-component ::color-component ::color-component))
 (s/def ::rect (s/tuple nat-int? nat-int? nat-int? nat-int? ::color))
 
+(defmacro with-valid-rect-data [data & body]
+  `(let [result# (s/conform ::rect ~data)]
+     (if (= ::s/invalid result#)
+       {:status 400
+        :body (str "could not read request body as rect data; "
+                   "spec output follows:\n"
+                   (s/explain-str ::rect ~data)
+                   \newline
+                   (pr-str (s/explain-data ::rect ~data)))}
+       (do ~@body))))
+
 (defn handle-rect-request
   [request]
-  (let [body-string (-> request :body slurp)
-        body-data (try
-                    (edn/read-string body-string)
-                    (catch Throwable _ nil))]
-    (if-not body-data
-      {:status 400,
-       :body (str "Could not read input string '" body-string "' as edn.\n"
-                  "Try something like this instead:\n"
-                  "[50 100 10 30 [0.95 0.5 0.1]]\n"
-                  "(i.e. x, y, width, height, and RGB color\n)")}
-
-      (let [rect-data (s/conform ::rect body-data)]
-        (if (= ::s/invalid rect-data)
-          {:status 400
-           :body (str "could not read request body as rect data; "
-                      "spec output follows:\n"
-                      (s/explain-str ::rect body-data)
-                      \newline
-                      (pr-str (s/explain-data ::rect body-data)))}
-
-          (let [[x y width height color] rect-data
-                [red green blue] color]
-            (canvas/draw-rect canvas x y width height red green blue)
-            {:status 200}
-            {:status 200}))))))
+  (with-verified-edn-body request [body-data]
+    (with-valid-rect-data body-data
+      (let [[x y width height color] body-data
+            [red green blue] color]
+        (canvas/draw-rect canvas x y width height red green blue)
+        {:status 200}))))
 
 (defn handler [request]
   (let [method (:request-method request)
