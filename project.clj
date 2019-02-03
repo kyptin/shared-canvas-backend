2bf7af5e1156edbe292c7758064d50bc90a478ed
Jeff Terrell <terrell@cs.unc.edu>
2019-02-02 20:32:46 -0500

    Transition from deps.edn to Leiningen
    Heroku doesn't support deps.edn projects (yet?).
    
    Also added a license to be clear that students and others can feel
    free to rip off this code.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject tech.jeffterrell.draw "0.1.0-SNAPSHOT"
+  :description "Demo backend for COMP 523 spring 2019"
+  :url "http://www.cs.unc.edu/~stotts/COMP523-s19/"
+  :license {:name "The MIT License", :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ring "1.7.0"]]
+  :repl-options {:init-ns tech.jeffterrell.draw.main})
