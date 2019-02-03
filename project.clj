9af862388e23a6cacda21dd682b13ef161490dd3
Jeff Terrell <terrell@cs.unc.edu>
2019-02-02 20:41:43 -0500

    Specify minimum Leiningen version of 2.0.0

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Demo backend for COMP 523 spring 2019"
   :url "http://www.cs.unc.edu/~stotts/COMP523-s19/"
   :license {:name "The MIT License", :url "https://opensource.org/licenses/MIT"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring "1.7.0"]]
   :uberjar-name "comp523-backend-standalone.jar"
