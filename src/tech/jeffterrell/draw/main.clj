b00a6d134d19dba062a4ead093177abd124a506b
Jeff Terrell <terrell@cs.unc.edu>
2019-02-02 20:15:42 -0500

    Remove `canvas` singleton
    Instead, create a new canvas when the server starts.

--- src/tech/jeffterrell/draw/main.clj
+++ src/tech/jeffterrell/draw/main.clj
@@ -5,10 +5,8 @@
             [tech.jeffterrell.draw.macros :refer [with-verified-edn-body
                                                   with-valid-rect-data]]))
 
-(defonce canvas (canvas/new-canvas))
-
 (defn handle-rect-request
-  [request]
+  [canvas request]
   (with-verified-edn-body request [body-data]
     (with-valid-rect-data body-data
       (let [[x y width height color] body-data
@@ -16,24 +14,26 @@
         (canvas/draw-rect canvas x y width height red green blue)
         {:status 200}))))
 
-(defn handler [request]
-  (let [method (:request-method request)
-        path (:uri request)]
-    (cond
-      (and (= method :get) (= path "/image"))
-      (canvas/canvas-as-png-response-map 200 canvas)
+(defn new-handler [canvas]
+  (fn [request]
+    (let [method (:request-method request)
+          path (:uri request)]
+      (cond
+        (and (= method :get) (= path "/image"))
+        (canvas/canvas-as-png-response-map 200 canvas)
 
-      (and (= method :post) (= path "/rect"))
-      (handle-rect-request request)
+        (and (= method :post) (= path "/rect"))
+        (handle-rect-request canvas request)
 
-      :else {:status 404})))
+        :else {:status 404}))))
 
 (defn start-server!
   ([] (start-server! false))
   ([join?]
    (def server
-     (jetty/run-jetty handler {:port (or (System/getenv "PORT") 4002)
-                               :join? join?}))))
+     (jetty/run-jetty (new-handler (canvas/new-canvas))
+                      {:port (or (System/getenv "PORT") 4002)
+                       :join? join?}))))
 
 (defn stop-server! []
   (.stop server)
